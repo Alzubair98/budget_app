baf160893849b2312f252886adcd7de4b246676d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-13 00:27:30 +0300

    add first files

---
+++ db/migrate/20221112211129_create_entities.rb
@@ -0,0 +1,9 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.integer :amount 
+      t.timestamps
+    end
+  end
+end
