a411dff4bd03f79d57e704021b410ff3acc72021
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-15 23:46:28 +0300

    add show pages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,12 @@
 class GroupsController < ApplicationController
     before_action :authenticate_user!
-    
+
     def index 
         @groups = Group.all 
+    end
+
+    def show 
+        @group = Group.find(params[:id])
     end
 
     def new 
